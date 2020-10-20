8eb536460f5f598bf45f5910cd3477ae3b1a32bd
ailtoncunhasilva <ailtoncunhasilva@hotmail.com>
2020-10-19 21:27:45 -0300

    First commit

---
+++ android/app/src/main/kotlin/br/com/cunha/legislacao_transito/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.cunha.legislacao_transito
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Legislação de trânsito',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          elevation: 0,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Guia do futuro condutor',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 26,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(12),
+                child: ListView(
+                  children: [
+                    textItem('Legislação de Trânsito'),
+                    Divider(height: 16),
+                    textItem('Direção Defensiva'),
+                    Divider(height: 16),
+                    textItem('Mecânica Básica'),
+                    Divider(height: 16),
+                    textItem('Meio Ambiente e Cidadania'),
+                    Divider(height: 16),
+                    textItem('Primeiros Socorros'),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(16),
+                child: Card(
+                  child: Text('texto ainda a digitar'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget textItem(String text) {
+  return GestureDetector(
+      child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          text,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+        Icon(Icons.arrow_forward_ios, size: 20),
+      ],
+    ),
+  );
+}
