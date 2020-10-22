44f35b1a8a484c3d214d1553f78568831b3d5789
ailtoncunhasilva <ailtoncunhasilva@hotmail.com>
2020-10-22 13:36:50 -0300

    layout changed

--- lib/main.dart
+++ lib/main.dart
@@ -31,112 +31,124 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red[900],
-        title: Text(
-          'Guia do futuro',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 22,
-            color: Colors.white,
-            //fontFamily: 'ArchitectsDaughter',
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              height: 80,
-              width: MediaQuery.of(context).size.width * 1.4,
-              color: Colors.yellow[700],
-              child: Center(
-                child: Text(
-                  'CONDUTOR',
-                  style: TextStyle(
-                    fontSize: 62,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Anton-Regular',
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(12),
-                child: ListView(
-                  children: [
-                    InkWell(
-                      child: textItem('Legislação de Trânsito'),
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => LegislacaoPage()));
-                      },
-                    ),
-                    Divider(height: 18),
-                    InkWell(
-                      child: textItem('Direção Defensiva'),
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => DefensivaPage()));
-                      },
-                    ),
-                    Divider(height: 18),
-                    InkWell(
-                      child: textItem('Mecânica Básica'),
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => MecanicaPage()));
-                      },
-                    ),
-                    Divider(height: 18),
-                    InkWell(
-                      child: textItem('Meio Ambiente e Cidadania'),
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => AmbientePage()));
-                      },
-                    ),
-                    Divider(height: 18),
-                    InkWell(
-                      child: textItem('Primeiros Socorros'),
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => SocorrosPage()));
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 1.2,
-                padding: EdgeInsets.all(16),
-                child: Card(
-                  elevation: 8,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(14),
-                  ),
-                  margin: EdgeInsets.all(16),
-                  color: Colors.green[800],
-                  child: Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: Center(
-                      child: Text(
-                        'Atualizado com a Lei 13.804, de 10 de janeiro/2019, que instituiu importantes mudanças no CTB (Código de Trânsito Brasileiro), prevendo a suspensão do direito de dirigir e a cassação da CNH do agente, quando for comprovada a utilização de veículo para fins da prática de contrabando, receptação e descaminho',
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                color: Colors.black,
+                height: 40,
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Guia do futuro',
                         style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
                           color: Colors.white,
-                          fontSize: 16,
+                        ),
+                      ),
+                      Icon(
+                        Icons.directions_car,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 80,
+                width: MediaQuery.of(context).size.width * 1.4,
+                color: Colors.yellow[700],
+                child: Center(
+                  child: Text(
+                    'CONDUTOR',
+                    style: TextStyle(
+                      fontSize: 62,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Anton-Regular',
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+                  child: ListView(
+                    children: [
+                      InkWell(
+                        child: textItem('Legislação de Trânsito'),
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => LegislacaoPage()));
+                        },
+                      ),
+                      Divider(height: 18),
+                      InkWell(
+                        child: textItem('Direção Defensiva'),
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => DefensivaPage()));
+                        },
+                      ),
+                      Divider(height: 18),
+                      InkWell(
+                        child: textItem('Mecânica Básica'),
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => MecanicaPage()));
+                        },
+                      ),
+                      Divider(height: 18),
+                      InkWell(
+                        child: textItem('Meio Ambiente e Cidadania'),
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => AmbientePage()));
+                        },
+                      ),
+                      Divider(height: 18),
+                      InkWell(
+                        child: textItem('Primeiros Socorros'),
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => SocorrosPage()));
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 1.2,
+                  padding: EdgeInsets.only(bottom: 16, left: 10, right: 10),
+                  child: Card(
+                    elevation: 8,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                    margin: EdgeInsets.all(16),
+                    color: Colors.green[800],
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Center(
+                        child: Text(
+                          'Atualizado com a Lei 13.804, de 10 de janeiro/2019, que instituiu importantes mudanças no CTB (Código de Trânsito Brasileiro), prevendo a suspensão do direito de dirigir e a cassação da CNH do agente, quando for comprovada a utilização de veículo para fins da prática de contrabando, receptação e descaminho',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
